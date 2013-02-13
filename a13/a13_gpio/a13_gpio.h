2204486930da41380e3adec7ce4e3cae67b5f9b6
Ilia <iliapaev@gmail.com>
2013-02-13 22:08:01 +0200

    a13_gpio: add header file and output

---
+++ a13/a13_gpio/a13_gpio.h
@@ -0,0 +1,21 @@
+#ifndef _A13_GPIO_H
+#define _A13_GPIO_H
+
+/*
+ * sunxi has 9 banks of gpio, they are:
+ * PA0 - PA17 | PB0 - PB23 | PC0 - PC24
+ * PD0 - PD27 | PE0 - PE31 | PF0 - PF5
+ * PG0 - PG9  | PH0 - PH27 | PI0 - PI12
+ */
+
+#define SUNXI_GPIO_A    0
+#define SUNXI_GPIO_B    1
+#define SUNXI_GPIO_C    2
+#define SUNXI_GPIO_D    3
+#define SUNXI_GPIO_E    4
+#define SUNXI_GPIO_F    5
+#define SUNXI_GPIO_G    6
+#define SUNXI_GPIO_H    7
+#define SUNXI_GPIO_I    8
+
+#endif /* _A13_GPIO_H */
